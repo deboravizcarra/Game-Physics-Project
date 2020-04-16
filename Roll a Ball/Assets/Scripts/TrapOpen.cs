a6dbf1a3b7b00babf5211accdfc7d98b917e0723
xEchoNitex <54558317+xEchoNitex@users.noreply.github.com>
2020-04-15 20:13:59 -0400

    Add files via upload
    Fan Rotate - Spins fan. Can change speed and direction in script 
    TrapOpen/TrapOpen2 - Script for trap floor pitfall.

---
+++ Roll a Ball/Assets/Scripts/TrapOpen.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapOpen : MonoBehaviour
+{
+    public GameObject Cube;
+
+        void OnTriggerEnter(){
+        Cube.GetComponent<Animation>().Play("TrapDoorAnim");
+    }
+}
