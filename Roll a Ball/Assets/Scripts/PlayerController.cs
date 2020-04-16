bbc746369bdfb2feb0e9c943988cd82048cf6d99
deboravizcarra <31870859+deboravizcarra@users.noreply.github.com>
2020-04-15 23:06:37 -0400

    fixed scripts

--- Roll a Ball/Assets/Scripts/PlayerController.cs
+++ Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -1,13 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed;
-    public Text countText;
-    public Text winText;
+    //public Text countText;
+    //public Text winText;
+
     private Rigidbody rb;
     private int count;
 
@@ -15,8 +15,8 @@ public class PlayerController : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         count = 0;
-        SetCountText();
-        winText.text = "";
+        //SetCountText();
+        //winText.text = "";
     }
 
     void FixedUpdate()
@@ -29,16 +29,16 @@ public class PlayerController : MonoBehaviour
         rb.AddForce(movement * speed);
     }
 
-    private void OnTriggerEnter(Collider other)
+    void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.CompareTag("Pick Up"))
+        if (other.gameObject.CompareTag("Pick Up"))
         {
             other.gameObject.SetActive(false);
             count = count + 1;
-            SetCountText();
+            //SetCountText();
         }
     }
-
+/*
     void SetCountText()
     {
         countText.text = "Count: " + count.ToString();
@@ -46,5 +46,5 @@ public class PlayerController : MonoBehaviour
         {
             winText.text = "You Win!";
         }
-    }
+    }*/
 }
