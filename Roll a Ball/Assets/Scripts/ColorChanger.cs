a6829cc0edbc8d20686d2b7f8acc3a5ca8908854
deboravizcarra <31870859+deboravizcarra@users.noreply.github.com>
2020-04-21 23:33:02 -0400

    color changing script

---
+++ Roll a Ball/Assets/Scripts/ColorChanger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChanger : MonoBehaviour
+{
+    public Material[] colors;
+    Renderer rend;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+        rend.sharedMaterial = colors[5];
+    }
+
+    public void newColor(int n)
+    {
+        for(int i = 0; i < colors.Length; i++)
+        {
+            if (i == n)
+                rend.sharedMaterial = colors[i];
+        }
+    }
+}
