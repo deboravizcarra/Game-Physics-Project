5a108ba0f1e8db0f7c6dfaf7fd470c241ea1f0a0
deboravizcarra <31870859+deboravizcarra@users.noreply.github.com>
2020-04-24 23:48:58 -0400

    settings/pause game

---
+++ Roll a Ball/Assets/Scripts/PauseGame.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour
+{
+    public static bool isPaused = false;
+    public GameObject menuUI;
+
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void Unpause()
+    {
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+}
