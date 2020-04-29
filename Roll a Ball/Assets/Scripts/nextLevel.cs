1c9138231ec5aded877eae5f141f38b12ed1dec3
deboravizcarra <31870859+deboravizcarra@users.noreply.github.com>
2020-04-28 21:21:39 -0400

    next level loading

---
+++ Roll a Ball/Assets/Scripts/nextLevel.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class nextLevel : MonoBehaviour
+{
+    public int nextScene;
+
+    void Start()
+    {
+        nextScene = SceneManager.GetActiveScene().buildIndex + 1;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            if(SceneManager.GetActiveScene().buildIndex == 3)
+            {
+                SceneManager.LoadScene(4);
+                PlayerPrefs.DeleteAll();
+            }
+            else
+            {
+                SceneManager.LoadScene(nextScene);
+
+                if (nextScene > PlayerPrefs.GetInt("levelAt"))
+                {
+                    PlayerPrefs.SetInt("levelAt", nextScene);
+                }
+            } 
+        }
+    }
+}
